1c6d6095cd0fb281d88f860844643488d4755ed2
sansu sa ma air <zun7441.yru@gmail.com>
2022-01-15 20:38:17 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+       <div class="container justify-center">
+           <h1 class="text-2xl text-green-900">TEST</h1>
+       </div>
+    </body>
+</html>
